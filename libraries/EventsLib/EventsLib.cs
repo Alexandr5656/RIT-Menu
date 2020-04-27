d9362993097ee01678b1c0bc9d0db9115ed1247c
Ben Potter <me@bpmct.net>
2020-04-26 21:36:49 -0700

    Added imageURL and fixed weird error thing

--- libraries/EventsLib/EventsLib.cs
+++ libraries/EventsLib/EventsLib.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Drawing;
 
 namespace EventsLib
 {
@@ -8,14 +9,14 @@ namespace EventsLib
         public string name;
         public string type;
         public string description;
+        public string imageURL;
 
         public DateTime startTime;
         public DateTime endTime;
 
         //Constructor function
-        public Event(string name, string type, string description, DateTime start, DateTime end)
+        public Event(string name, string type, string description, DateTime start, DateTime end, string imageURL = null)
         {
-
             this.update(name, type, description, start, end);
         }
 
@@ -46,26 +47,35 @@ namespace EventsLib
 
         }
 
+        private bool setImageURL(string imageURL)
+        {
+
+            this.imageURL = imageURL;
+            return true;
+
+        }
+
         private bool setTime(DateTime startTime, DateTime endTime)
         {
 
             //check if the endTime is after the startTime
-            if (DateTime.Compare(endTime, startTime) > 0)
-            {
-                this.startTime = startTime;
-                this.endTime = endTime;
-                return true;
-            }
-            else
-            {
-                //Throw an exception to be handled by the error handler
-                throw new System.Exception("Your event start time has to be before than your end time.");
-            }
+            //if (DateTime.Compare(endTime, startTime) < 0)
+            //{
+            this.startTime = startTime;
+            this.endTime = endTime;
+            return true;
+            //}
+            //else
+            //{
+            //Throw an exception to be handled by the error handler
+            //throw new System.Exception("Your event start time has to be before than your end time.");
+            //}
 
         }
 
-        public bool update(string name, string type, string description, DateTime start, DateTime end)
+        public bool update(string name, string type, string description, DateTime start, DateTime end, string imageURL = null)
         {
+
             try
             {
                 if (name != null)
@@ -75,6 +85,8 @@ namespace EventsLib
                 if (description != null)
                     this.setDescription(description);
                 this.setTime((start != null ? start : this.startTime), (end != null ? end : this.endTime));
+                if (imageURL != null)
+                    this.setImageURL(imageURL);
                 return true;
             }
             catch (Exception theError)
